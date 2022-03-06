09d3c0153594d905e5d572d6facad06b17866da2
vanvans03-def <you@example.com>
2022-03-06 18:52:54 +0700

    add iphone shop page

--- main.js
+++ main.js
@@ -17,3 +17,26 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+var lastClickedFace = null;
+
+const divs = document.querySelectorAll('.rom');
+Array.from(divs).forEach(div => {
+    div.addEventListener('click', classToggler);
+});
+
+const colors = ['forest', 'field', 'beach', 'mine', 'ocean'];
+let	enumerator = 0;
+
+function classToggler() {
+if (enumerator < colors.length+1){
+  	enumerator+=1;
+  }
+ else {enumerator=0};
+
+
+this.classList.add(colors[enumerator]);
+this.classList.remove(colors[enumerator-1]);
+const yellows = document.querySelectorAll('.beach')
+const infoLabel = document.querySelector('#info')
+infoLabel.innerHTML = "There are " + yellows.length + " yellow box(es)." 
+};
