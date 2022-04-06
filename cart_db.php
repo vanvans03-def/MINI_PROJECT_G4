5f6b7b72759ebfa9ac14dee994e8ab39d42c344b
vanvans03-def <teerahitchana@gmail.com>
2022-04-06 20:06:00 +0700

    do the order page

--- cart_db.php
+++ cart_db.php
@@ -4,36 +4,36 @@ require_once "admin/config/db.php";
 
 
 
-$quantity = 1;  
 if (isset($_POST['submitbag'])) {
     date_default_timezone_set("Asia/Bangkok");
     $dttm = Date("Y-m-d G:i:s");  
     $userid = $_SESSION['id'];
     $pdid =  $_SESSION['order.id'];
-    $quantity = 1;
-  
+
 
 }else{
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    //header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
-
-  
+$quan  =1 ;
+//addcart
 $sql = $conn->prepare("INSERT INTO cart_item ( `user_id`,`product_id`,`date`,`quantity`)VALUES (:user,:product_id,:dttm,:quantity)");
 $sql->bindParam(":user",$userid); 
 $sql->bindParam(":product_id",$pdid );
 $sql->bindParam(":dttm", $dttm);
-$sql->bindParam(":quantity", $quantity);
+$sql->bindParam(":quantity", $quan);
 $sql->execute();
  
 if ($sql) {
     $_SESSION['success'] = "Add Product to cart successfully";
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    header('location: order.php');
   
 } else {
     $_SESSION['errorshop'] = "ไม่สำเร็จ";
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 }
+//add order
+
 
 ?>
 
@@ -47,7 +47,9 @@ if ($sql) {
 </head>
 <body>
     <h1>
+ <?php 
  
+?>
     </h1>
 </body>
 </html>
